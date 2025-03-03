7a1b17806e1c816ad15b95934f268775570849e5
Ian Chen <chenian317@gmail.com>
2025-03-03 16:50:54 -0300

    script server de debug frontend del api

---
+++ raspberry_pi/scripts/test_post_server.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+
+api = FastAPI()
+
+class LedSpeedLevel(BaseModel):
+    level: int
+
+
+# Permitir CORS desde el origen http://127.0.0.1 (frontend)
+api.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1"],  # O usa ["*"] para permitir todos los orígenes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos HTTP
+    allow_headers=["*"],  # Permitir todos los headers
+)
+
+
+# Request POST *SIN* BODY
+@api.post("/api/led/change_effect")
+def change_effect():
+    return "changing effect"
+
+
+@api.post("/api/led/change_speed")
+def change_speed(speed: LedSpeedLevel):
+    print("GOT INPUT SPEED: ", speed.level)
+    return {'velocidad': speed.level}
+
