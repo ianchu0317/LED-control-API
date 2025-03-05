844116adb8fa58d297ac5a245b40c56913fc39ad
Ian Chen <chenian317@gmail.com>
2025-03-05 18:10:32 -0300

    añadir detención de servidor web

--- raspberry_pi/start.sh
+++ raspberry_pi/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+printf "[+] INICIANDO SERVIDORES\n"
+printf "Inciando servidor web...\n"
 sudo service apache2 start
 
+printf "\nActivando entorno virtual...\n"
+source ./api/.venv/bin/activate
+
+printf "\nIniciando servidor API...\n"
 fastapi run ./api/main.py
+
+printf "\nDesactivando entorno virtual...\n"
+deactivate
+
+printf "\nDeteniendo servidor web...\n"
+sudo service apache2 stop
+
+printf "\n[+]SERVIDORES TERMINADOS\n"
